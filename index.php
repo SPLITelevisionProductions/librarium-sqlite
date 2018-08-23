bcc3e2fee0e94a4717d8a3eb7d78198b173f6e2b
Logan Burrell <loganeaburrell@gmail.com>
2018-08-23 15:25:55 +1200

    Beginnings of registration, moved some functions to the index page (like logout, register)

--- index.php
+++ index.php
@@ -9,8 +9,35 @@ if (isset($_GET['u'])) {
     header($header);
 } elseif($user->isAuthd()) {
     // If the user is logged in, direct to their collection
-    header('Location: collection');
-} elseif(!$user->isAuthd()) {
+	header('Location: collection');
+} elseif(isset($_GET['logout'])) {
+	$user->logOut();
+} elseif(isset($_GET['register'])) {
+	// Define Page Title and initial class
+    $pageTitle = 'Register';
+	$pageClass = 'register system';
+	//$webAppTesting = true;
+	require_once('styles/php/header.php');?>
+
+<header>
+	<div class="diagtitle">Registration</div>
+</header>
+<div class="vertAlignWrap">
+	<div class="vertAlignCont">
+		<div id="RGTerms" class="current">
+			<h1>Welcome to Librarium.</h1>
+			<p>This app/website allows you to keep track of the Books, CDs, Games, DVDs, or other collectable items that you own.</p>
+			<p>You can also loan and share the details of these items, as well as whole collections we call "Shelves".<p>
+			<p>By continuing, you agree to our <a href="#">Terms &amp; Conditions</a>, as well as our <a href="#">Privacy Policy</a>.<p>
+			<input type="button" id="RGTCContinue" class="btnForward" onclick="" value="Continue" tabindex="1">
+			<input type="button" id="RGTCReturn" class="btnBack" onclick="window.location='/'" value="Return to Sign In" tabindex="2">
+			<small>Note: If you have a login on a previous version of Librarium., just use those details to sign into this version</small>
+		</div>
+	</div>
+</div>
+<footer id="LoginFooter"></footer>
+
+<?} elseif(!$user->isAuthd()) {
     // If the user is not logged in, and no username is set, show a login page
 
     // Define Page Title and initial class
@@ -22,8 +49,8 @@ if (isset($_GET['u'])) {
 	$loginMsgClass = "";
 	$loginMsgText = "";
 
-	$loginMsgClass = "success";
-	$loginMsgText = "Testing success";
+	//$loginMsgClass = "success";
+	//$loginMsgText = "Testing success";
 	
 	if(isset($_GET['action'])){
 
@@ -36,6 +63,10 @@ if (isset($_GET['u'])) {
 			case 'active':
 				$loginMsgClass = "success";
 				$loginMsgText = "Account activated";
+				break;
+			case 'out':
+				$loginMsgClass = "success";
+				$loginMsgText = "Logged out successfully";
 				break;
 			case 'reset':
 				$loginMsgClass = "success";
@@ -64,7 +95,6 @@ if (isset($_GET['u'])) {
     // BELOW IS PULLED FROM V2 AS A BASIS, NOT FOR PRODUCTION USE
 ?>
 
-<!-- The header will not be used for the login screen, but we have a blank one for compatibility -->
 <header id="MainHeader" class="<?=$loginMsgClass?>"><div class="diagtitle"><?=$loginMsgText?></div></header>
 <div class="vertAlignWrap">
 	<div class="vertAlignCont">
@@ -89,7 +119,7 @@ if (isset($_GET['u'])) {
 </div>
 <footer id="LoginFooter">
 	<div class="forgotbtn ftrleft">Forgot Password</div>
-    <div class="registerbtn ftrright">Register</div>
+    <div class="registerbtn ftrright" onclick="window.location='/register'">Register</div>
 </footer>
 
 <?}?>
